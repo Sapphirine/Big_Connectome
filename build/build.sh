421b062eb711f0cc2bd8971591e30c17aca08f6f
terry <terry@ganita.org>
2015-05-19 12:22:36 -0400

    Updated the build script for smoother build process and automatic download of ganitagraph and ganitazero software

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# download ganitagraph and ganitazero first
+cd src/GRAPH/C++/
+git clone http://cloud.ganita.org:8083/ganita/ganitagraph.git
+git clone http://cloud.ganita.org:8083/ganita/ganitazero.git
+cd ../../../
+
+# start the build process
 cd src/
 sh ./build.sh
 cd ../
@@ -7,6 +14,8 @@ mkdir -p bin/
 cp src/PREP/c/h5dump2seq bin/
 cp src/PREP/cpp/getKasthuri11 bin/
 cp src/FEAT/C++/g3d bin/
-cp src/GRAPH/C++/GanitaGraph/ganitaGraph bin/
-cp src/GRAPH/C++/GanitaGraph/gg2 bin/
+cp src/GRAPH/C++/ganitagraph/build/ganitagraph1 bin/
+cp src/GRAPH/C++/ganitagraph/build/ganitagraph2 bin/
+cp src/GRAPH/C++/ganitagraph/build/gg3 bin/
+cp src/GRAPH/C++/ganitazero/build/gzero bin/
 cp src/PREP/Java/GanitaBasic.jar bin/
